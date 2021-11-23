482666e22302ad3267fe38cf21f00b15f0d0967f
MBRzealand <mikk24f6@edu.easj.dk>
2021-11-23 14:08:27 +0100

    added itemscheme.js added assets folder     - added placeholder_image.png updated routes.js

---
+++ Server/model/itemscheme.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"A name must be specified"],
+    },
+    image:{
+        type: String,
+        default: "../assets/placeholder_image.png",
+    },
+    specifications:[{ specification: String, value: String }],
+
+    price: {
+        type: Number,
+        required: [true,"A price must be specified"],
+        min:0,
+    },
+    inStorage: {
+        type: Number,
+        default: 0,
+        min:0,
+    },
+    amountSold: {
+        type: Number,
+        default: 0,
+        min:0,
+    },
+    categories:[String],
+
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
