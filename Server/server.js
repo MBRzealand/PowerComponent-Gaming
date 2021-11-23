4383fe7ec55b62e9ef7381996cc181772574d46c
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-11-23 11:42:15 +0100

    Renamed the serverTest folder to test to it gets the right lcon, added a dot env file

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const routes = require("./routes/routes");
+const express = require('express');
+const routes = require('./routes/routes');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use("/version1" ,routes);
-app.listen(3000,() => {
-    console.log("Server is running..");
+app.use('/version1', routes);
+app.listen(process.env.PORT, () => {
+  console.log('Server is running..');
 });
 
 module.exports = app;
