bfac82300b50cae27e941a535d60155973809e99
Chilas Main PC <Chil0041@edu.easj.dk>
2021-11-23 11:08:43 +0100

    test af første route og første route made

---
+++ Server/ServerTest/server.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const server = require('../server');
+
+describe(' Route', () => {
+  it('should return Json object with name Fredrik', () => {
+    return request(server)
+      .get('/version1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+         expect.objectContaining({
+             name: "Fredrik"
+         })
+          )});
+      });
+  
+
+
+    });
+
+
+ 
---
+++ Server/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/',function(req,res,next) {
+    res.status(200).json({
+        name : "Fredrik",
+    },)
+})
+
+module.exports = router;
