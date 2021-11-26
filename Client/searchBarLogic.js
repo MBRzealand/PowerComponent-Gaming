b27989f1a2e1303fe27bc580f050baf2b1fabeb3
MBRzealand <mikk24f6@edu.easj.dk>
2021-11-26 14:04:08 +0100

    Added search functionality for specifications and categories

--- Client/searchBarLogic.js
+++ Client/searchBarLogic.js
@@ -35,8 +35,6 @@ let displaySpecifications = (specificationsObjectArray) => {
 let generateCards = (response) => {
 
     const nameArrayLength = (response.foundElements.name.length);
-    const specificationsArrayLength = (response.foundElements.specifications.length);
-    const categoriesArrayLength = (response.foundElements.categories.length);
 
     for (let i = 0; i < nameArrayLength; i++) {
         const productName = document.createElement("h4")
@@ -96,5 +94,130 @@ let generateCards = (response) => {
 
     }
 
+    const line = document.createElement("hr")
+    resultsDiv.appendChild(line)
+
+    const specificationsArrayLength = (response.foundElements.specifications.length);
+
+    for (let i = 0; i < specificationsArrayLength; i++) {
+        const productName = document.createElement("h4")
+        const name = document.createTextNode(response.foundElements.name[i].name)
+        productName.appendChild(name)
+
+        const specificationTitle = document.createElement("h4")
+        const title = document.createTextNode("Specifications:")
+        specificationTitle.appendChild(title)
+
+        const productSpecifications = document.createElement("p")
+
+        for (let j = 0; j < response.foundElements.name[i].specifications.length*2; j+=2) {
+
+            let specificationList = displaySpecifications(response.foundElements.name[i].specifications)
+
+            let specificationKeyValuePair = document.createElement("p")
+
+            let keyValuePair = document.createTextNode(`${specificationList[j]}: ${specificationList[j+1]}`)
+
+            specificationKeyValuePair.appendChild(keyValuePair)
+
+            productSpecifications.appendChild(specificationKeyValuePair)
+        }
+
+        const categoriesTitle = document.createElement("h4")
+        const title2 = document.createTextNode("Categories:")
+        categoriesTitle.appendChild(title2)
+
+        const productCategories = document.createElement("p")
+
+        for (let j = 0; j < response.foundElements.name[i].categories.length; j++) {
+            const categories = document.createTextNode(`${response.foundElements.name[i].categories[j]}`)
+            const breakpoint = document.createElement("br")
+            productCategories.appendChild(categories)
+            productCategories.appendChild(breakpoint)
+        }
+
+        const priceTitle = document.createElement("h4")
+        const title3 = document.createTextNode("Price:")
+        priceTitle.appendChild(title3)
+
+        const productPrice = document.createElement("p")
+        const price = document.createTextNode(`${response.foundElements.name[i].price} kr`)
+        productPrice.appendChild(price)
+
+        const productCard = document.createElement("div")
+        productCard.appendChild(productName)
+        productCard.appendChild(specificationTitle)
+        productCard.appendChild(productSpecifications)
+        productCard.appendChild(categoriesTitle)
+        productCard.appendChild(productCategories)
+        productCard.appendChild(priceTitle)
+        productCard.appendChild(productPrice)
+
+        resultsDiv.appendChild(productCard)
+
+    }
+
+    resultsDiv.appendChild(line)
+
+    const categoriesArrayLength = (response.foundElements.categories.length);
+
+
+    for (let i = 0; i < specificationsArrayLength; i++) {
+        const productName = document.createElement("h4")
+        const name = document.createTextNode(response.foundElements.name[i].name)
+        productName.appendChild(name)
+
+        const specificationTitle = document.createElement("h4")
+        const title = document.createTextNode("Specifications:")
+        specificationTitle.appendChild(title)
+
+        const productSpecifications = document.createElement("p")
+
+        for (let j = 0; j < response.foundElements.name[i].specifications.length*2; j+=2) {
+
+            let specificationList = displaySpecifications(response.foundElements.name[i].specifications)
+
+            let specificationKeyValuePair = document.createElement("p")
+
+            let keyValuePair = document.createTextNode(`${specificationList[j]}: ${specificationList[j+1]}`)
+
+            specificationKeyValuePair.appendChild(keyValuePair)
+
+            productSpecifications.appendChild(specificationKeyValuePair)
+        }
+
+        const categoriesTitle = document.createElement("h4")
+        const title2 = document.createTextNode("Categories:")
+        categoriesTitle.appendChild(title2)
+
+        const productCategories = document.createElement("p")
+
+        for (let j = 0; j < response.foundElements.name[i].categories.length; j++) {
+            const categories = document.createTextNode(`${response.foundElements.name[i].categories[j]}`)
+            const breakpoint = document.createElement("br")
+            productCategories.appendChild(categories)
+            productCategories.appendChild(breakpoint)
+        }
+
+        const priceTitle = document.createElement("h4")
+        const title3 = document.createTextNode("Price:")
+        priceTitle.appendChild(title3)
+
+        const productPrice = document.createElement("p")
+        const price = document.createTextNode(`${response.foundElements.name[i].price} kr`)
+        productPrice.appendChild(price)
+
+        const productCard = document.createElement("div")
+        productCard.appendChild(productName)
+        productCard.appendChild(specificationTitle)
+        productCard.appendChild(productSpecifications)
+        productCard.appendChild(categoriesTitle)
+        productCard.appendChild(productCategories)
+        productCard.appendChild(priceTitle)
+        productCard.appendChild(productPrice)
+
+        resultsDiv.appendChild(productCard)
+
+    }
 
 }
